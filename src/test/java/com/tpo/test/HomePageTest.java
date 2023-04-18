c67fcfd278f6bf0d7815443ec94426529050bf3e
Vinay Singh <vinay.singh2@dhl.com>
2023-04-18 15:45:23 +0530

    CHECKING

--- src/test/java/com/tpo/test/HomePageTest.java
+++ src/test/java/com/tpo/test/HomePageTest.java
@@ -37,7 +37,7 @@ public class HomePageTest {
       Assert.assertEquals(finalString, "Apple iPhone");;
 	}
 	
-	@Test(priority = 5)
+	@Test(priority = 6)
 	public void closeBrowsers()
 	{
 		driver.quit();
